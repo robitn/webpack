6999fc459fb9fc377cc8b1a47209fa721e8df600
robitn <tim.n.robinson@gmail.com>
2016-07-15 14:41:45 -0500

    added autoprefixer to webpack.base.conf.js, again!

--- template/build/webpack.base.conf.js
+++ template/build/webpack.base.conf.js
@@ -76,10 +76,15 @@ module.exports = {
           limit: 10000,
           name: utils.assetsPath('fonts/[name].[hash:7].[ext]')
         }
+      },
+      {
+          test: /\.css$/,
+          loader: "style-loader!css-loader!postcss-loader"
       }
     ]
   },
   {{#lint}}
+  postcss: [ autoprefixer({ browsers: ['last 2 versions'] }) ],
   eslint: {
     formatter: require('eslint-friendly-formatter')
   },
